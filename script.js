d0459af3b1ffafd10a18dd8ff7aebb1eec92f083
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2019-12-30 22:39:40 -0500

    Daily eather function figured out

--- script.js
+++ script.js
@@ -1,100 +1,152 @@
+//$(document).ready(function() {
+
+    var query5DayURL = "http://api.openweathermap.org/data/2.5/forecast?q=philadelphia,us&units=imperial&APPID=b2af0c249ef1580d9d26aa8ca64187be"
+
+    function dailyWeather(input) {
+        
+        $("#icon").empty();
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + input + ",us&APPID=b2af0c249ef1580d9d26aa8ca64187be";
+        
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+
+        }).then(function(response){
 
 
-var query5DayURL = "http://api.openweathermap.org/data/2.5/forecast?q=philadelphia,us&units=imperial&APPID=b2af0c249ef1580d9d26aa8ca64187be"
+            console.log(response);
 
-function dailyWeather(input) {
-    
-    // $("#dailyDiv").empty();
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + input + ",us&APPID=b2af0c249ef1580d9d26aa8ca64187be";
-    
-    $.ajax({
-        url: queryURL,
-        method: "GET"
+            //Heading
+            var dailyDiv = $("#dailyDiv");
 
-    }).then(function(response){
+            var city = response.name;
+            var date = response.dt * 1000;
+            var dateTimeString = moment(date).format("MM-DD-YYYY");
+            var iconCode = JSON.stringify(response.weather[0].icon);
+            iconCode = iconCode.replace("\"","");
+            iconCode = iconCode.replace("\"","");
+            //console.log(iconCode);
+            var iconURL = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+            //console.log(icon);
+            var icon = $("<img>").attr("src", iconURL)
+            var dailyHeading = $("#cityDateIcon").text(city + " (" + dateTimeString + ") ");
+            dailyDiv.append(dailyHeading);
+            $("#icon").append(icon);
 
+            //Other parts
+            var temperature = ((response.main.temp - 273.15) * 9/5 + 32).toFixed(0);
+            $("#dailyTemp").text("Temperature: " + temperature);
+        
+            var humidity = response.main.humidity;
+            $("#dailyHumidity").text("Humidity: " + humidity + "%");
+             //console.log(humidity);
 
-        //console.log(response);
-
-        //Heading
-        var dailyDiv = $("#dailyDiv");
-
-        var city = response.name;
-        var date = response.dt * 1000;
-        var dateTimeString = moment(date).format("MM-DD-YYYY");
-        var iconCode = JSON.stringify(response.weather[0].icon);
-        iconCode = iconCode.replace("\"","");
-        iconCode = iconCode.replace("\"","");
-        //console.log(iconCode);
-        var iconURL = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
-        //console.log(icon);
-        var icon = $("<img>").attr("src", iconURL)
-        var dailyHeading = $("#cityDateIcon").text(city + " (" + dateTimeString + ") ");
-        dailyDiv.append(dailyHeading);
-        $("#icon").append(icon);
-
-        //Other parts
-        var temperature = ((response.main.temp - 273.15) * 9/5 + 32).toFixed(0);
-       $("#dailyTemp").text("Temperature: " + temperature);
-       
-       var humidity = response.main.humidity;
-       $("#dailyHumidity").text("Humidity: " + humidity + "%");
-       //console.log(humidity);
-
-
-
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
 
         
 
-    
+            UVIndex(lon, lat);
+            //return(lat, lon);
+            //uv call after longitude and lat is pulled
+            //pass long and lat as var to fill in url for UV call.
+        });
+    };
 
+    fiveDayForecast();
+
+    function fiveDayForecast() {
+        $.ajax({
+            url: query5DayURL,
+            method: "GET"
+
+        }).then(function(response) {
+
+            var dailyArray = response.list;
+            var idArray = [1, 2, 3, 4, 5];
+            var listArray = 8;
+           
+            console.log(response);
+
+            
+            for (var i = 0; i < idArray.length; i++) {
+            
+                var idString = JSON.stringify(idArray[i]);
+                console.log(idString);
+
+                var idArrayIndex = idArray.indexOf(i) + 1;
+                //console.log(jNew);
+            
+                // for (var j = 8; j < dailyArray.length; j+=8){
+                // }
+
+
+                    var tempString = response.list[listArray].main.temp.toFixed(0);
+                    console.log(tempString);
+                    var temp = JSON.stringify(tempString);
+                    temp = temp.replace("\"","");
+                    temp = temp.replace("\"","");
+                    
+                    
+                $("#Day-" + idString).children("#temp").text(temp);
+                listArray+=8;
+
+            }   
+        });
+    };
+            //var id = 1;
+            
+            //console.log(id);
+            
+            
+            
+            
+            
+            
+            // for (j=0; j < idArray.length; i++) { 
+                
+                //     var idString = JSON.stringify(idArray[j].value);
+                //     var id = $("#Day-" + idString);
+                //     console.log(id);
+                //    //$(id).children(".description").val(message);
+                
+                
+
+
+
+            //for loop pulls what is needed and appends all in one swoop.
+
+        //});
+
+
+    function UVIndex(lon, lat){
         
 
+        var queryUVIndex = "http://api.openweathermap.org/data/2.5/uvi?appid=b2af0c249ef1580d9d26aa8ca64187be&lat=" + lat + "&lon=" + lon;
 
-        var lat = 0;
-        var lon = 0;
+        $.ajax({
 
-        //UVIndex();
-        //return(lat, lon);
-        //uv call after longitude and lat is pulled
-        //pass long and lat as var to fill in url for UV call.
+            url: queryUVIndex,
+            method: "GET"
+        }).then(function(response){
+            console.log(response);
+
+
+
+            uvIndex = response.value;
+            $("#dailyUVIndex").text("UV Index: " + uvIndex);
+        });
+    };
+
+    //On Click
+
+    $("#searchButton").on("click", function(event){
+        event.preventDefault();
+
+        var input = $("#input").val().trim();
+        dailyWeather(input);
+
+
     });
- };
 
-
-function fiveDayForecast() {
-    $.ajax({
-        url: query5DayURL,
-        method: "GET"
-
-    }).then(function(response){
-
-        //for (var i = 8, i+=8
-
-        //for loop pulls what is needed and appends all in one swoop.
-
-        console.log(response);
-    });
-}
-
-
-function UVIndex(){
-
-    var queryUVIndex = "http://api.openweathermap.org/data/2.5/uvi?appid=b2af0c249ef1580d9d26aa8ca64187be&lat=" + lat + "&lon=" + lon;
-
-    $.ajax({
-
-        url
-    })
-}
-
-//On Click
-
-$("#searchButton").on("click", function(event){
-    event.preventDefault();
-
-    var input = $("#input").val().trim();
-    dailyWeather(input);
-
-
-});
+//});
